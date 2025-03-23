83263d3c3250ab3b6f6fb9b0293a08ed45a13f1b
Paul <frostfyre@gmail.com>
2025-03-23 13:51:36 -0700

    adding validation functions
    list PNGs, list AVIFs, etc

--- get_modeling_frames.py
+++ get_modeling_frames.py
@@ -71,7 +71,8 @@ def process_frames(frames_root, camera_numbers, frame_number, out_dir):
 
 if __name__ == '__main__':
     # Example usage
-    frames_root = '/Users/spooky/Downloads/LA-data-frames/Model2/EXP_eye_neutral/'  # Root directory containing camera folders
+    # frames_root = '/Users/spooky/Downloads/LA-data-frames/Model2/EXP_eye_neutral/'  # Root directory containing camera folders
+    frames_root = '/mnt/data/datasets/LA-data-frames/Model2/EXP_eye_neutral/'  # Root directory containing camera folders
     camera_numbers = ['camera_01', 'camera_02', 'camera_03', 'camera_04',
                       'camera_08', 'camera_09','camera_10', 'camera_11',
                       'camera_12','camera_13','camera_14','camera_15',
@@ -84,7 +85,9 @@ if __name__ == '__main__':
                       'camera_68', 'camera_87', ]  # List of camera numbers to process 60-63
 
     frame_number = '00099'  # Frame number to look for
-    out_dir = '/Users/spooky/Developer/facebuilder/Auto-Only-Plugin/alt_source/'  # Directory to save processed frames
+    # out_dir = '/Users/spooky/Developer/facebuilder/Auto-Only-Plugin/alt_source/'  # Directory to save processed frames
+    out_dir = '~/modeling_frames/Model2/EXP_eye_neutral/'  # Directory to save processed frames
+  # Directory to save processed frames
 
     process_frames(frames_root, camera_numbers, frame_number, out_dir)
     logger.info('Frame processing completed.')
